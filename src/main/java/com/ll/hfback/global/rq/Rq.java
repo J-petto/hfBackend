27bd2e3db6b78b036ae7b161a6c1904441145341
황민지 <hminji4565@gmail.com>
2025-02-05 14:21:33 +0900

    cookie

--- src/main/java/com/ll/hfback/global/rq/Rq.java
+++ src/main/java/com/ll/hfback/global/rq/Rq.java
@@ -7,6 +7,7 @@ import com.ll.hfback.global.security.SecurityUser;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,21 +29,24 @@ public class Rq {
   private final HttpServletResponse resp;
   private final AuthService authService;
 
+  @Value("${custom.cookie.domain}")
+  private String domain;
+
 
   public void setLogin(Member member) {
     UserDetails user = new SecurityUser(
-        member.getId(),
-        member.getEmail(),
-        "",
-        member.getNickname(),
-        member.getProfilePath(),
-        member.getAuthorities()
+            member.getId(),
+            member.getEmail(),
+            "",
+            member.getNickname(),
+            member.getProfilePath(),
+            member.getAuthorities()
     );
 
     Authentication authentication = new UsernamePasswordAuthenticationToken(
-        user,
-        user.getPassword(),
-        user.getAuthorities()
+            user,
+            user.getPassword(),
+            user.getAuthorities()
     );
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -51,66 +55,66 @@ public class Rq {
 
   public Member getActor() {
     return Optional.ofNullable(
-            SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-        )
-        .map(Authentication::getPrincipal)
-        .filter(principal -> principal instanceof SecurityUser)
-        .map(principal -> (SecurityUser) principal)
-        .map(securityUser -> {
-          String roleName = securityUser.getAuthorities()
-              .stream()
-              .findFirst()
-              .map(GrantedAuthority::getAuthority)
-              .orElse(MemberRole.ROLE_USER.name());
-          MemberRole role = MemberRole.valueOf(roleName);
+                    SecurityContextHolder
+                            .getContext()
+                            .getAuthentication()
+            )
+            .map(Authentication::getPrincipal)
+            .filter(principal -> principal instanceof SecurityUser)
+            .map(principal -> (SecurityUser) principal)
+            .map(securityUser -> {
+              String roleName = securityUser.getAuthorities()
+                      .stream()
+                      .findFirst()
+                      .map(GrantedAuthority::getAuthority)
+                      .orElse(MemberRole.ROLE_USER.name());
+              MemberRole role = MemberRole.valueOf(roleName);
 
-          return new Member(
-              securityUser.getId(),
-              securityUser.getUsername(),
-              securityUser.getNickname(),
-              securityUser.getProfilePath(),
-              role
-          );
-        })
-        .orElse(null);
+              return new Member(
+                      securityUser.getId(),
+                      securityUser.getUsername(),
+                      securityUser.getNickname(),
+                      securityUser.getProfilePath(),
+                      role
+              );
+            })
+            .orElse(null);
   }
 
 
   public void setCookie(String name, String value) {
     ResponseCookie cookie = ResponseCookie.from(name, value)
-        .path("/")
-        .domain("localhost")
-        .sameSite("Strict")
-        .secure(true)
-        .httpOnly(true)
-        .build();
+            .path("/")
+            .domain(domain)
+            .sameSite("Strict")
+            .secure(true)
+            .httpOnly(true)
+            .build();
     resp.addHeader("Set-Cookie", cookie.toString());
   }
 
 
   public String getCookieValue(String name) {
     return Optional
-        .ofNullable(req.getCookies())
-        .stream() // 1 ~ 0
-        .flatMap(cookies -> Arrays.stream(cookies))
-        .filter(cookie -> cookie.getName().equals(name))
-        .map(cookie -> cookie.getValue())
-        .findFirst()
-        .orElse(null);
+            .ofNullable(req.getCookies())
+            .stream() // 1 ~ 0
+            .flatMap(cookies -> Arrays.stream(cookies))
+            .filter(cookie -> cookie.getName().equals(name))
+            .map(cookie -> cookie.getValue())
+            .findFirst()
+            .orElse(null);
   }
 
 
   public void deleteCookie(String name) {
     ResponseCookie cookie = ResponseCookie.from(name, null)
-        .path("/")
-        .domain("localhost")
-        .sameSite("Strict")
-        .secure(true)
-        .httpOnly(true)
-        .maxAge(0)
-        .build();
+            .path("/")
+            .domain(domain)
+            .sameSite("Strict")
+            .secure(true)
+            .httpOnly(true)
+            .maxAge(0)
+            .build();
     resp.addHeader("Set-Cookie", cookie.toString());
   }
 
